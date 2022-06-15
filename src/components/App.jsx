29be42171a5b07a31ae03926e67aca9ed0c06377
Vancomm <vanushnick@gmail.com>
2022-06-15 23:55:45 +0300

    implemented

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,23 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable no-param-reassign */
 /* eslint-disable react/no-array-index-key */
-import React, { useCallback, useRef, useState } from 'react';
+import React, {
+  useCallback, useRef, useState,
+} from 'react';
 import produce from 'immer';
-import Cell from './Cell.jsx';
+import cn from 'classnames';
 
-/* TODO: rewrite using state from hooks (like Ben Awad) */
-
+const CELL_SIZE = '20px';
 const NUM_ROWS = 30;
 const NUM_COLS = 30;
 const MS_TIMEOUT = 250;
+const RND_SPARSITY = 0.8;
 
-const neighbourVectors = [
+const vectors = [
   [-1, -1],
   [-1, 0],
   [-1, 1],
-  [-1, 0],
   [0, -1],
   [0, 1],
   [1, -1],
@@ -22,66 +25,133 @@ const neighbourVectors = [
   [1, 1],
 ];
 
+const getEmptyField = () => Array(NUM_ROWS).fill(Array(NUM_COLS).fill(0));
+
+const getRandomField = () => Array.from(
+  Array(NUM_ROWS),
+  () => Array.from(
+    Array(NUM_COLS),
+    () => (Math.random() > RND_SPARSITY ? 1 : 0),
+  ),
+);
+
 export default function App() {
   const [running, setRunning] = useState(false);
 
-  const [field, setField] = useState(
-    () => Array(NUM_ROWS).fill(Array(NUM_COLS).fill(0)),
-  );
+  const [field, setField] = useState(getEmptyField);
 
   const runningRef = useRef();
   runningRef.current = running;
 
-  const runNextGen = useCallback(() => {
+  const runSimulation = useCallback(() => {
     if (!runningRef.current) return;
 
-    setField((f) => produce(f, (c) => {
+    setField((f) => produce(f, (fieldCopy) => {
       for (let i = 0; i < NUM_ROWS; i += 1) {
         for (let j = 0; j < NUM_COLS; j += 1) {
-          const neighbours = neighbourVectors.map(([dx, dy]) => {
-            const x = i + dx;
-            const y = j + dy;
-          });
+          const neighbours = vectors
+            .map(([dx, dy]) => {
+              const x = i + dx;
+              const y = j + dy;
+              if (x >= 0 && x < NUM_ROWS && y >= 0 && y < NUM_COLS) {
+                return f[x][y];
+              }
+              return 0;
+            })
+            .reduce((sum, add) => sum + add, 0);
+
+          if (neighbours < 2 || neighbours > 3) {
+            fieldCopy[i][j] = 0;
+          } else if (f[i][j] === 0 && neighbours === 3) {
+            fieldCopy[i][j] = 1;
+          }
         }
       }
     }));
 
-    setTimeout(runNextGen, MS_TIMEOUT);
+    setTimeout(runSimulation, MS_TIMEOUT);
   }, []);
 
   const toggleRunning = () => {
     setRunning(!running);
+    if (!running) {
+      runningRef.current = true;
+      runSimulation();
+    }
+  };
+
+  const clearField = () => {
+    setField(getEmptyField());
+  };
+
+  const randomizeField = () => {
+    setField(getRandomField());
   };
 
   const toggleCell = (i, j) => () => {
-    const newField = produce(field, (fieldCopy) => {
+    setField((f) => produce(f, (fieldCopy) => {
       fieldCopy[i][j] = fieldCopy[i][j] === 0 ? 1 : 0;
-    });
-    setField(newField);
+    }));
   };
+
+  // useEffect(() => {
+  //   const handleKeyDown = (e) => {
+  //     switch (e.key) {
+  //       case 's': {
+  //         e.preventDefault();
+  //         toggleRunning();
+  //         break;
+  //       }
+  //       case 'c': {
+  //         e.preventDefault();
+  //         clearField();
+  //         break;
+  //       }
+  //       case 'r': {
+  //         e.preventDefault();
+  //         randomizeField();
+  //         break;
+  //       }
+  //       default: break;
+  //     }
+  //   };
+  //   document.addEventListener('keydown', handleKeyDown);
+  //   return () => {
+  //     document.removeEventListener('keydown', handleKeyDown);
+  //   };
+  // }, []);
 
   return (
     <>
       <h1>Conway&apos;s Game of Life</h1>
-      <div id="buttons">
+      <div className="buttons">
         <button
           type="button"
           onClick={toggleRunning}
         >
           {running ? 'Stop' : 'Start'}
         </button>
+        <button
+          type="button"
+          onClick={clearField}
+        >
+          Clear
+        </button>
+        <button
+          type="button"
+          onClick={randomizeField}
+        >
+          Random
+        </button>
       </div>
       <div
-        className="field"
-        style={{
-          '--num-rows': NUM_ROWS,
-          '--num-cols': NUM_COLS,
-        }}
+        className={cn('field', running ? 'running' : undefined)}
+        style={{ '--num-rows': NUM_ROWS, '--num-cols': NUM_COLS, '--cell-size': CELL_SIZE }}
       >
         {field.map((row, i) => row.map((value, j) => (
-          <Cell
+          <div
             key={`cell-${i}-${j}`}
-            alive={value}
+            className={cn('cell', (value === 1 ? 'alive' : 'dead'))}
             onClick={toggleCell(i, j)}
           />
         )))}
--- src/slices/index.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import fieldReducer from './fieldSlice.js';
-
-export default configureStore({
-  reducer: {
-    field: fieldReducer,
-  },
-});
